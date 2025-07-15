def8ad13808c5a7cf198d90be085259bc36a52f8
Lucas Romero <74377571+Lucasrb17@users.noreply.github.com>
2025-07-15 13:25:52 -0300

    feat: adding add to cart test

---
+++ tests/add-to-cart.spec.ts
@@ -0,0 +1,26 @@
+import { test } from '@playwright/test';
+import { HomePage } from '../pages/HomePage';
+import { ProductsPage } from '../pages/ProductsPage';
+import { CartPage } from '../pages/CartPage';
+
+test('Agregar producto al carrito sin estar logueado', async ({ page }) => {
+  const home = new HomePage(page);
+  const products = new ProductsPage(page);
+  const cart = new CartPage(page);
+
+  await home.navigate();
+  await home.goToProducts();
+
+  await products.verifyLoaded();
+  await products.viewFirstProduct();
+  await products.verifyProductDetail();
+
+  await products.setQuantity(1); // O 2 si querés testear más cantidad
+  await products.addToCart();
+  await products.continueShopping();
+
+  await cart.openCart();
+  await cart.verifyProductInCart('Blue Top');
+  await cart.proceedToCheckout();
+  await cart.verifyGuestCheckoutPrompt();
+});
